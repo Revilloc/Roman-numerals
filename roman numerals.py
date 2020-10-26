618c0b546a15edebfe97880156629f26d4070dbd
Revilloc <56065165+Revilloc@users.noreply.github.com>
2020-10-26 21:23:14 +0000

    Patch 4
    Turned into function

--- roman numerals.py
+++ roman numerals.py
@@ -1,26 +1,28 @@
-number = int(input("Please enter the number you want to be converted into roman numerals: "))
-count = number
-numeral = ''
-while count > 0:
-    if count >= 1000:
-        numeral += 'M'
-        count -= 1000
-    elif count >= 500:
-        numeral += 'D'
-        count -= 500
-    elif count >= 100:
-        numeral += 'C'
-        count -= 100
-    elif count >= 50:
-        numeral += 'L'
-        count -= 50
-    elif count >= 10:
-        numeral += 'X'
-        count -= 10
-    elif count >= 5:
-        numeral += "V"
-        count -= 5
-    elif count >= 1:
-        numeral += "I"
-        count -= 1                              
-print(str(number) + " in roman numerals is " + str(numeral))
+def numerals(number):
+    count = number
+    numeral = ''
+    while count > 0:
+        if count >= 1000:
+            numeral += 'M'
+            count -= 1000
+        elif count >= 500:
+            numeral += 'D'
+            count -= 500
+        elif count >= 100:
+            numeral += 'C'
+            count -= 100
+        elif count >= 50:
+            numeral += 'L'
+            count -= 50
+        elif count >= 10:
+            numeral += 'X'
+            count -= 10
+        elif count >= 5:
+            numeral += "V"
+            count -= 5
+        elif count >= 1:
+            numeral += "I"
+            count -= 1
+    return str(number) + " is " + str(numeral) + " in roman numerals"
+
+print(numerals(int(input("Please enter the number you want to be converted into roman numerals: "))))
